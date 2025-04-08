111f053700196e5ea973569379a0ddef5b05706a
Subinho <stepansubrt@icloud.com>
2025-04-08 11:56:01 +0200

    a jumbotron

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
-      <Footer />
+      <Hero />
+      {/* <Footer /> */}
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import HeroImg from "../assets/images/hero.jpg";
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='hero-container'>
+            <div className='hero-left'>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>Savor fresh, vibrant flavors in every bite—where simple ingredients shine</p>
+                <Link to="#" aria-label="On click">Reserve table</Link>
+            </div>
+            <div className='hero-right'>
+                <img src={HeroImg} alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
